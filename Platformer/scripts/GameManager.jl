60e13806e95e2d007707b075fe8407c13d3fb421
Kyle Conel <kyjordev@gmail.com>
2023-12-26 16:15:27 -0500

    UI elements and starting a second scene

--- Platformer/scripts/GameManager.jl
+++ Platformer/scripts/GameManager.jl
@@ -17,7 +17,11 @@ function Base.getproperty(this::GameManager, s::Symbol)
         function()
             MAIN.scene.camera.offset = JulGame.Math.Vector2f(0, -2.75)
             MAIN.cameraBackgroundColor = [30, 111, 80]
-            this.parent.addComponent(ShapeModule.Shape(Math.Vector2(10,5), Math.Vector3(0), true; isWorldEntity=false, position=Math.Vector2(0,0)))
+            this.parent.addComponent(ShapeModule.Shape(Math.Vector2(10,5), Math.Vector3(0), true; isWorldEntity=false, position=Math.Vector2f(1.05,0.7)))
+            MAIN.scene.getEntityByName("CoinUI").getSprite().isWorldEntity = false
+            MAIN.scene.getEntityByName("CoinUI").getSprite().position = JulGame.Math.Vector2f(-.1, 1)
+            MAIN.scene.getEntityByName("LivesUI").getSprite().isWorldEntity = false
+            MAIN.scene.getEntityByName("LivesUI").getSprite().position = JulGame.Math.Vector2f(-.1, .25)
         end
     elseif s == :update
         function(deltaTime)
