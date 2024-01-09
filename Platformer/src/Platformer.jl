47b3e700c9762e60d1ffe52da63179edc7541b1a
Kyle Conel <kyjordev@gmail.com>
2024-01-08 20:48:50 -0500

    Frame rate changed to 60

--- Platformer/src/Platformer.jl
+++ Platformer/src/Platformer.jl
@@ -5,7 +5,7 @@ module Platformer
     
     function run()
         scene = Scene("level_0.json")
-        return scene.init("JulGame Example", false, Vector2(),Vector2(1280, 720), true, 1.0, true, 144)
+        return scene.init("JulGame Example", false, Vector2(),Vector2(1280, 720), true, 1.0, true, 60)
     end
 
     julia_main() = run()
